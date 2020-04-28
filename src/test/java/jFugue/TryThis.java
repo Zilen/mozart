8c7626db04f49cccad3ed5caff8d3dd0ef4eb120
Alexandre Praxedes <az.praxedes@gmail.com>
2020-04-27 21:04:29 -0300

    remoção testes jFugue e upversion POM

--- src/test/java/jFugue/TryThis.java
+++
@@ -1,9 +0,0 @@
-package jFugue;
-
-
-public class TryThis {
-//    public static void main(String[] args) {
-//        new Player().play(new ChordProgression("I IV vi V").eachChordAs("$!i $!i Ri $!i"), new Rhythm().addLayer("..X...X...X...XO"));
-//        System.out.println("!");
-//    }
-}
